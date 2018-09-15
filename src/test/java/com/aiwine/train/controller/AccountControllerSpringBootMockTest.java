34424d672c480dbc3aa6b9c0240c5fb4058a1144
PhatSriwichai <phattharaphon.sriwichai@gmail.com>
2018-09-15 14:39:37 +0700

    add jacksonTester

--- src/test/java/com/aiwine/train/controller/AccountControllerSpringBootMockTest.java
+++ src/test/java/com/aiwine/train/controller/AccountControllerSpringBootMockTest.java
@@ -1,10 +1,13 @@
 package com.aiwine.train.controller;
 
+import com.aiwine.train.controller.response.AccountResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
@@ -25,14 +28,21 @@ public class AccountControllerSpringBootMockTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private JacksonTester<AccountResponse> jacksonTester;
+
     @Test
     public void getById() throws Exception {
+
+        JacksonTester.initFields(this, new ObjectMapper());
+
         MockHttpServletResponse response = mockMvc.perform(
                 MockMvcRequestBuilders.get("/account/1")
                         .accept(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
 
         assertEquals(HttpStatus.OK.value(), response.getStatus());
-        assertEquals("", response.getContentAsString());
+
+        AccountResponse expected = new AccountResponse("AAA", "aaa", 100000);
+        assertEquals(jacksonTester.write(expected).getJson() , response.getContentAsString());
     }
 }
